7ffd8705fa345dd8d82a1e3d473ccc99e7c2dd08
Adrian Lagarto <adrianlagartomc@gmail.com>
2025-01-05 23:41:48 -0800

    adjust_db_class_Appointment_model

--- server/models.py
+++ server/models.py
@@ -7,6 +7,7 @@ class Appointment(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(15), nullable=False)
     message = db.Column(db.Text, nullable=True)
     price = db.Column(db.Float, nullable=False)
     date = db.Column(db.String(50), nullable=False)

972bbca919335ee3713c4baa789ffb8dd5c2348c
Adrian Lagarto <adrianlagartomc@gmail.com>
2025-01-27 23:23:04 -0800

    modify_frontend_admin_in_clent

--- server/routes/Booknow.py
+++ server/routes/Booknow.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 
 Booknow = Blueprint('Booknow', __name__)
 
-# get this data from admin client
+# Initial schedule data
 schedule_data = [
     {"date": "2025-01-20", "time": "10:00", "price": 50},
     {"date": "2025-01-18", "time": "14:00", "price": 75},
@@ -11,6 +11,13 @@ schedule_data = [
 @Booknow.route('/booknow', methods=['GET'])
 def get_schedule():
     return jsonify(schedule_data)
+
+@Booknow.route('/booknow', methods=['POST'])
+def update_schedule():
+    global schedule_data
+    data = request.get_json()
+    schedule_data = data  # Update the schedule data
+    return jsonify({"message": "Schedule updated successfully"}), 200
 
 @Booknow.route('/payment', methods=['POST'])
 def process_payment():

29e3084601f439be8b8b79bf396ca989fd4b6740
Adrian Lagarto <adrianlagartomc@gmail.com>
2025-02-06 23:43:12 -0800

    contact_routes

--- server/main.py
+++ server/main.py
@@ -21,7 +21,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins="*")
+CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- server/routes/Contact.py
+++ server/routes/Contact.py
@@ -1,37 +1,42 @@
 from flask import Blueprint, jsonify, request
-# from main import db
-from models import ContactForm
+from models import ContactForm, db
+from flask_cors import CORS, cross_origin
 
 Contact = Blueprint('Contact', __name__)
+# CORS(Contact, resources={r"/*": {"origins": "*"}}) 
 
-@Contact.route('/Contact', methods=['GET','POST'])
+@Contact.route('/Contact', methods=['GET', 'POST'])
+@cross_origin()
 def contact():
-    # Contact Us Form
-    if request.method == 'POST':
-        if not request.is_json:
-            return jsonify({"error": "Request body must be JSON"}), 400
+    try:
+        if request.method == 'POST':
+            if not request.is_json:
+                return jsonify({"error": "Request body must be JSON"}), 400
+            
+            data = request.get_json()
+            first_name = data.get('first_name')
+            last_name = data.get('last_name')
+            email = data.get('email')
+            message = data.get('message')
+            
+            print(f'first_name: {first_name}')
+            print(f'last_name: {last_name}')
+            print(f'message: {message}')
+            print(f'email: {email}')
+            
+            # new_contact = ContactForm(
+            #     first_name=first_name,
+            #     last_name=last_name,
+            #     email=email,
+            #     message=message
+            # )
+            
+            # db.session.add(new_contact)
+            # db.session.commit()
+            # ISSUE with DB connection 
+          
+            return jsonify({"message": "Message received!"}), 200
         
-        data = request.get_json()
-        first_name = data.get('first_name')
-        last_name = data.get('last_name')
-        email = data.get('email')
-        message = data.get('message')
-        
-        new_contact = ContactForm(
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            message=message
-        )
-        
-        # db.session.add(new_contact)
-        # db.session.commit()
-      
-        return jsonify({"message": "Message received!"}), 200
-    
-    # if request.method == 'Get':
-        
-    #     return jsonify({"message": "Get Rquest Contact routes received!"}), 200
-    
-      
-    return jsonify({"message": "Get Request Contact routes received!"}), 200
+        return jsonify({"message": "Get Request Contact routes received!"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500

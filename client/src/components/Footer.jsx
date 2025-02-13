618ba73c0cdf85fc4b933a96eccad16a54a08b67
Adrian Lagarto <adrianlagartomc@gmail.com>
2025-02-12 23:09:12 -0800

    footer_scss

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,9 +4,13 @@ import "./Footer.scss"; // Import SCSS file
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>
-        &copy; 2025 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </p>
+      <div className="footer-content">
+        <p>
+          &copy; 2025 Your Company Name. All rights reserved. |{" "}
+          <a href="/privacy-policy">Privacy Policy</a> |{" "}
+          <a href="/terms-of-service">Terms of Service</a>
+        </p>
+      </div>
     </footer>
   );
 };
